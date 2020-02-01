208a60ff29c2fdedd0ac86272d79ffcd0dc219de
Jeff Bauer (RIT Student) <geoffersthethird@gmail.com>
2020-02-01 11:20:38 -0500

    The legs work I guess

---
+++ Assets/Scripts/Testing/TestImpulse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestImpulse : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 impulse = Vector3.zero;
+
+    new private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(impulse, ForceMode.Impulse);
+            Debug.Log("Applied " + impulse + " to " + rigidbody.gameObject.name);
+        }
+    }
+}
