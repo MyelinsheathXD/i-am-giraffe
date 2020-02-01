77bc24b0f058eed1370d258bdd44b2a90c020484
unknown <IGMAdmin@MSS2150-07.main.ad.rit.edu>
2020-02-01 17:02:32 -0500

    Added 2 items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -15,12 +15,46 @@ public class Item : MonoBehaviour
     [SerializeField]
     private float breakImpulse = 2f;
 
+    [SerializeField]
+    private GameObject brokenPrefab = null;
+
+    private bool broken = false;
+
+    private Rigidbody rigidBody = null;
+
+    private Vector3 preVelocity;
+    private Vector3 preAngularVelocity;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        preVelocity = rigidBody.velocity;
+        preAngularVelocity = rigidBody.angularVelocity;
+    }
+
+    private void FixedUpdate()
+    {
+        preVelocity = rigidBody.velocity;
+        preAngularVelocity = rigidBody.angularVelocity;
+    }
+
     private void Break()
     {
+        if (broken) return;
+        broken = true;
+        GameObject newObject = Instantiate(brokenPrefab, this.transform.position, this.transform.rotation);
+
+        Rigidbody[] pieces = newObject.GetComponentsInChildren<Rigidbody>();
+        foreach(Rigidbody p in pieces)
+        {
+            p.velocity = preVelocity;
+            p.angularVelocity = preAngularVelocity;
+        }
+
         Destroy(this.gameObject);
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         float impulseMagnitude = collision.impulse.magnitude;
         if (impulseMagnitude >= breakImpulse)
