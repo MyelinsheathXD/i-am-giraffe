2f6a57c07a318fa402d4728e9d720d8043242577
Jeff Bauer (RIT Student) <geoffersthethird@gmail.com>
2020-01-31 22:46:58 -0500

    Floating segmented giraffe body

---
+++ Assets/Scripts/Character/FaceDirection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDirection : MonoBehaviour
+{
+    new private Rigidbody rigidbody;
+
+    [SerializeField]
+    private Vector3 bodyForward = new Vector3(0, 0, 2);
+
+    [SerializeField]
+    private Vector3 facingDirection = Vector3.zero;
+
+    [SerializeField]
+    private float facingForce = 800;
+
+    [SerializeField]
+    private float leadTime = 0;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        facingDirection = facingDirection.normalized;
+        bodyForward = bodyForward.normalized;
+
+        if (leadTime == 0)
+        {
+            if (facingDirection != Vector3.zero)
+            {
+                Vector3 force = facingForce * facingDirection * Time.fixedDeltaTime;
+                Vector3 forcePosition = rigidbody.transform.TransformDirection(bodyForward);
+                rigidbody.AddForceAtPosition(force, forcePosition, ForceMode.Impulse);
+                rigidbody.AddForceAtPosition(-force, -forcePosition, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            Vector3 targetPoint = transform.position + facingDirection * bodyForward.magnitude;
+            Vector3 currentPoint = transform.TransformPoint(bodyForward);
+            Vector3 reversePoint = transform.TransformPoint(-bodyForward);
+            Vector3 velocity = rigidbody.GetPointVelocity(currentPoint);
+
+            Vector3 diff = targetPoint - (currentPoint + velocity * leadTime);
+
+            rigidbody.AddForceAtPosition(facingForce * diff * Time.fixedDeltaTime, currentPoint, ForceMode.Impulse);
+            rigidbody.AddForceAtPosition(-facingForce * diff * Time.fixedDeltaTime, reversePoint, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/TargetHeight.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHeight : MonoBehaviour
+{
+    [SerializeField]
+    private float targetHeight = 1f;
+
+    [SerializeField]
+    private float strength = 0f;
+
+    [SerializeField]
+    private float leadTime = 0.3f;
+
+    [SerializeField]
+    private Transform inRelationTo = null;
+
+    [SerializeField]
+    private LayerMask mask = 0;
+
+    new private Rigidbody rigidbody = null;
+    private float groundHeight = 0;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, Vector3.down, out hit, 100f, mask))
+        {
+            groundHeight = hit.point.y;
+        }
+
+        float diff = (groundHeight + targetHeight) - (this.transform.position.y + rigidbody.velocity.y * leadTime);
+        if (inRelationTo != null) {
+            diff = inRelationTo.TransformPoint(Vector3.up * targetHeight).y - (transform.position.y + rigidbody.velocity.y * leadTime);
+        }
+
+        float distance = Mathf.Abs(diff);
+        float pullM = Mathf.Clamp01(distance / 0.3f);
+        rigidbody.AddForce(new Vector3(0, Mathf.Sign(diff) * strength * pullM * Time.fixedDeltaTime, 0), ForceMode.Impulse);
+    }
+}
