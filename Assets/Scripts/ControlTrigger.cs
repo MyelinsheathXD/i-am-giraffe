1bcf3e5fb1c3b171be4dd94161216d0f7ff2c90f
Jeff Bauer (RIT Student) <geoffersthethird@gmail.com>
2020-02-02 14:53:39 -0500

    level 1 controls

---
+++ Assets/Scripts/ControlTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteDisplayer spriteDisplayer = null;
+
+    [SerializeField]
+    private Sprite spr1 = null;
+
+    [SerializeField]
+    private Sprite spr2 = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        spriteDisplayer.DisplaySprite(spr1, spr2);
+        Destroy(this.gameObject);
+    }
+}
