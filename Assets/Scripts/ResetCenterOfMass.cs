7eaa977c7c2cf383777a3821ebef7d5f2c074d3d
Jeff Bauer (RIT Student) <geoffersthethird@gmail.com>
2020-02-02 10:54:40 -0500

    Neck is fine.  Trust me:

---
+++ Assets/Scripts/ResetCenterOfMass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCenterOfMass : MonoBehaviour
+{
+    private void Awake()
+    {
+        Rigidbody rbody = GetComponent<Rigidbody>();
+        if (rbody != null)
+        {
+            rbody.ResetCenterOfMass();
+        }
+    }
+}
