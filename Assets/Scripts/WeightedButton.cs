9a3383fb3d9bff350655d47e107bc9b96f4c3b89
Jeff Bauer (RIT Student) <geoffersthethird@gmail.com>
2020-02-02 12:24:40 -0500

    Level 3

---
+++ Assets/Scripts/WeightedButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedButton : MonoBehaviour
+{
+    [SerializeField]
+    private Color defaultColor = Color.black;
+
+    [SerializeField]
+    private GameObject[] lights;
+
+    [SerializeField]
+    private GameObject cage;
+
+    private Color[] originalColors;
+
+    private int count = 0;
+
+    private void Awake()
+    {
+        originalColors = new Color[lights.Length];
+        for (int i = 0; i < lights.Length; i++)
+        {
+            originalColors[i] = lights[i].GetComponent<Renderer>().material.color;
+            lights[i].GetComponent<Renderer>().material.color = defaultColor;
+        }
+        StartCoroutine(Activate());
+    }
+
+    private IEnumerator Activate()
+    {
+        float targetHeight = 13f;
+        while (cage.transform.position.y < targetHeight)
+        {
+            cage.transform.position += Vector3.up * 4f * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void Add()
+    {
+        if (count >= lights.Length) return;
+        lights[count].GetComponent<Renderer>().material.color = originalColors[count];
+        count++;
+        if (count == lights.Length) StartCoroutine(Activate());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Item"))
+        {
+            Add();
+        }
+    }
+}
